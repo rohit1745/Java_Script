e1c43d00acbc7e0bdd6ffab68db1f7ddfab6e737
Rohit <rohitbadgujar535@gmail.com>
2026-08-04 18:10:27 +0530

    JS

---
+++ Strings/prac.js
@@ -0,0 +1,16 @@
+let fullName = "       Rohit Badgujar        ";
+console.log(fullName);
+
+fullName = fullName.trim();     //remove the unused space 
+console.log(fullName);  
+
+fullName = fullName.toUpperCase();  //to lower case
+console.log(fullName);
+
+fullName = fullName.toLocaleLowerCase();       //to upper case
+console.log(fullName);
+
+fullName = fullName.indexOf()   
+console.log(fullName);
+
+
---
+++ Strings/string.js
@@ -0,0 +1,38 @@
+let firstName = 'Rohit '
+let lastName = 'Badgujar'
+console.log(`Hello ${firstName}`);
+
+console.log('===============================');
+
+//indxing in string 
+let A = ['hello buddy']
+console.log(A);
+
+
+console.log('=================================');
+
+
+let a = 100;
+let t = String(a)
+console.log(a);
+console.log(t);
+
+console.log('====================================');
+
+let c='1000'
+let numm = Boolean(c)
+let boolean = Boolean("Hello")  //true
+console.log(c);
+console.log(numm);
+console.log(boolean);
+
+let bool = Boolean('') //false becaue is not declare any value (null , '' , "" undefine)
+console.log(bool);
+
+
+
+
+console.log('=========================');
+
+
+
